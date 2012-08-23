4ccdfeb6603c83842050431d293b382ec7bfcdb6
Marcus <git@noveis.net>
2012-08-22 20:48:59 -0700

    Actually uses keys. Suitable for some use. WordPress readme.

--- encrypted_blog.php
+++ encrypted_blog.php
@@ -3,13 +3,13 @@
 Plugin Name: Encrypted Blog
 Plugin URI: https://github.com/marcusds/EncryptedBlog
 Description: A brief description of the Plugin.
-Version: 0.0.2
+Version: 0.0.3.1
 Author: marcusds
 Author URI: https://github.com/marcusds
 License: GPL2
 */
 
-/*  Copyright 2012  Marcus Schwab
+/*  Copyright 2012  Marcus S.
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
@@ -32,10 +32,10 @@ class encryptblog {
 	 *
 	 * @return text HTML content describing embedded figure
 	 **/
-	function decrypt_content($val)
-	{
-		$key = "This is a very secret key";
-		$val = encryptblog::encdec($val, $key);
+	function decrypt_content($val) {
+		if( isset( $_SESSION['encryption_key'] ) ) {	
+			$val = encryptblog::encdec( $val, $_SESSION['encryption_key'] );
+		}
 		return $val;
 	}
 
@@ -44,76 +44,80 @@ class encryptblog {
 	 *
 	 * @return text HTML content describing embedded figure
 	 **/
-	function encrypt_content($val)
-	{
-		$key = "This is a very secret key";
-		$val = encryptblog::encdec($val, $key);
+	function encrypt_content( $val ) {
+		if( isset( $_SESSION['encryption_key'] ) ) {	
+			$val = encryptblog::encdec( $val, $_SESSION['encryption_key'] );
+		}
 		return $val;
 	}
 
-	function encdec($str,$ky='')
-	{
-		if ($ky == '')
-		{
+	function encdec( $str, $key = '' ) {
+		if ($key == '') {
 			return $str;
 		}
-		$ky = str_replace(chr(32), '', $ky);
-		if(strlen($ky) < 8)
+		$key = str_replace( chr( 32 ), '', $key );
+		if( strlen($key) < 8 )
 		{
-			exit('key error');
+			if( isset( $_SESSION['encryption_key'] ) ) {
+				unset( $_SESSION['encryption_key'] );
+			}
+			return 'key error';
 		}
-		$kl = strlen($ky) <32 ? strlen($ky) : 32;
+		$kl = strlen( $key ) < 32 ? strlen( $key ) : 32;
 		
 		$k = array();
-		for($i = 0; $i < $kl; $i++)
-		{
-			$k[$i] = ord($ky{$i})&0x1F;
+		for($i = 0; $i < $kl; $i++) {
+			$k[$i] = ord( $key{$i} )&0x1F;
 		}
 		$j = 0;
-		for($i = 0; $i < strlen($str); $i++)
-		{
-			$e = ord($str{$i});
-			$str{$i} = $e&0xE0 ? chr($e ^$k[$j]) : chr($e);
+		for($i = 0; $i < strlen($str); $i++) {
+			$e = ord( $str{$i} );
+			$str{$i} = $e&0xE0 ? chr( $e ^ $k[$j] ) : chr( $e );
 			$j++;
 			$j = $j == $kl ? 0 : $j;
 		}
 		return $str;
 	}
 
-	function eb_redirect($redirect_to, $request)
-	{
+	function start_session() {
+		if( ! session_id() ) {
+			session_start();
+		}
+		if( isset( $_POST['encryption_key'] ) ) {
+			$_SESSION['encryption_key'] = $_POST['encryption_key'];
+		}
+	}
+
+	function end_session() {
+		session_destroy();
+	}	
+ 
+	// @param $template - Full path to the normal template file. 
+	function key_get_template($template) { 
+		if( is_user_logged_in() && ! isset( $_SESSION['encryption_key'] ) ) {
+			return dirname( __FILE__ ) . '/encrypted_blog_form.php';
+		} else {
+			return $template;
+		}
+	} 
+	
+	function login_redirect($redirect_to, $request){
 		global $current_user;
 		get_currentuserinfo();
 		//is there a user to check?
 		if(is_array($current_user->roles))
 		{
-			//check for admins
-			if(in_array("administrator", $current_user->roles))
-				return home_url("/wp-admin/");
-			else
-				return home_url();
+			return home_url(); // We need to always redirect somewhere where it'll check the template so that we can fire key_get_template() and get the user's key.
 		}
-	}
-
-	function eb_startsession()
-	{
-		if(!session_id())
-		{
-			session_start();
-		}
-	}
-
-	function eb_endsession()
-	{
-		session_destroy();
 	}
 }
 
-add_filter('the_content', array('encryptblog', 'decrypt_content'), 1, 1);
-add_filter('content_edit_pre', array('encryptblog', 'decrypt_content'), 1, 1);
-add_filter('content_save_pre', array('encryptblog', 'encrypt_content'), 1, 1);
-add_filter('login_redirect', array('encryptblog', 'eb_redirect'), 10, 3);
-add_filter('init', array('encryptblog', 'eb_startsession'), 1);
-add_action('wp_logout', array('encryptblog', 'eb_endsession'));
-add_action('wp_login', array('encryptblog', 'eb_endsession'));
+add_filter( 'the_content', array( 'encryptblog', 'decrypt_content' ), 1, 1 );
+add_filter( 'content_edit_pre', array( 'encryptblog', 'decrypt_content' ), 1, 1 );
+add_filter( 'content_save_pre', array( 'encryptblog', 'encrypt_content' ), 1, 1 );
+add_filter( 'template_include', array( 'encryptblog', 'key_get_template' ), 1, 1 );
+add_filter( 'init', array( 'encryptblog', 'start_session' ), 1 );
+add_filter( 'login_redirect', array( 'encryptblog', 'login_redirect' ), 10, 3 );
+add_action( 'wp_logout', array( 'encryptblog', 'end_session' ) );
+add_action( 'wp_login', array( 'encryptblog', 'end_session' ) );
 ?>
--- readme.txt
+++ readme.txt
@@ -1,6 +1,23 @@
-TODO
+=== Encrypted Blog ===
+Contributors: marcus1060 
+Tags: security,encryption,private
+Requires at least: 3.4.1
+Tested up to: 3.4.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Require uses be logged in to view site.
-On login redirect to form that user enters encoding key.
-Create table that tracks if entries are encoded or not.
-Add option to bulk encode or permantly decode entries.
+Completely encrypts blog posts so that even with access to the WordPress database your posts will be private.
+
+== Description ==
+
+Completely encrypts blog posts so that even with access to the WordPress database your posts will be private.
+This plugin is in ALPHA! Be incredibly careful in your use of it.
+It will now encrypt old blog posts and will probably just wreck them will trying to decrypt them.
+If you want to try this plugin out best to do so on a new install, works nicely then at least.
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
+== Changelog ==
+View https://github.com/marcusds/EncryptedBlog/commits/master for changelog.
