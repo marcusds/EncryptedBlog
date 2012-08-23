e4ae557ca9429990b95496c7d5a73784b27d42d2
Marcus <git@noveis.net>
2012-08-22 19:55:57 -0700

    Change to class, add todo.

--- encrypted_blog.php
+++ encrypted_blog.php
@@ -3,7 +3,7 @@
 Plugin Name: Encrypted Blog
 Plugin URI: https://github.com/marcusds/EncryptedBlog
 Description: A brief description of the Plugin.
-Version: 0.0.1
+Version: 0.0.2
 Author: marcusds
 Author URI: https://github.com/marcusds
 License: GPL2
@@ -25,59 +25,95 @@ License: GPL2
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-add_filter('the_content', 'decrypt_content',1,1);
-add_filter('content_edit_pre', 'decrypt_content',1,1);
+class encryptblog {
 
-/**
- * Filter to replace the [caption] shortcode text with HTML5 compliant code
- *
- * @return text HTML content describing embedded figure
- **/
-function decrypt_content($val)
-{
-    $key = "This is a very secret key";
-	$val = encdec($val, $key);
-	return $val;
+	/**
+	 * Filter to replace the [caption] shortcode text with HTML5 compliant code
+	 *
+	 * @return text HTML content describing embedded figure
+	 **/
+	function decrypt_content($val)
+	{
+		$key = "This is a very secret key";
+		$val = encryptblog::encdec($val, $key);
+		return $val;
+	}
+
+	/**
+	 * Filter to replace the [caption] shortcode text with HTML5 compliant code
+	 *
+	 * @return text HTML content describing embedded figure
+	 **/
+	function encrypt_content($val)
+	{
+		$key = "This is a very secret key";
+		$val = encryptblog::encdec($val, $key);
+		return $val;
+	}
+
+	function encdec($str,$ky='')
+	{
+		if ($ky == '')
+		{
+			return $str;
+		}
+		$ky = str_replace(chr(32), '', $ky);
+		if(strlen($ky) < 8)
+		{
+			exit('key error');
+		}
+		$kl = strlen($ky) <32 ? strlen($ky) : 32;
+		
+		$k = array();
+		for($i = 0; $i < $kl; $i++)
+		{
+			$k[$i] = ord($ky{$i})&0x1F;
+		}
+		$j = 0;
+		for($i = 0; $i < strlen($str); $i++)
+		{
+			$e = ord($str{$i});
+			$str{$i} = $e&0xE0 ? chr($e ^$k[$j]) : chr($e);
+			$j++;
+			$j = $j == $kl ? 0 : $j;
+		}
+		return $str;
+	}
+
+	function eb_redirect($redirect_to, $request)
+	{
+		global $current_user;
+		get_currentuserinfo();
+		//is there a user to check?
+		if(is_array($current_user->roles))
+		{
+			//check for admins
+			if(in_array("administrator", $current_user->roles))
+				return home_url("/wp-admin/");
+			else
+				return home_url();
+		}
+	}
+
+	function eb_startsession()
+	{
+		if(!session_id())
+		{
+			session_start();
+		}
+	}
+
+	function eb_endsession()
+	{
+		session_destroy();
+	}
 }
 
-add_filter('content_save_pre', 'encrypt_content',1,1);
-
-/**
- * Filter to replace the [caption] shortcode text with HTML5 compliant code
- *
- * @return text HTML content describing embedded figure
- **/
-function encrypt_content($val)
-{
-    $key = "This is a very secret key";
-	$val = encdec($val, $key);
-	return $val;
-}
-
-function encdec($str,$ky='')
-{
-	if($ky=='')return $str;
-	
-	$ky = str_replace(chr(32),'',$ky);
-	if(strlen($ky)<8)
-	{
-		exit('key error');
-	}
-	$kl = strlen($ky )<32?strlen($ky):32;
-	
-	$k = array();
-	for($i=0;$i<$kl;$i++)
-	{
-		$k[$i] = ord($ky{$i})&0x1F;
-	}
-	$j = 0;
-	for($i=0;$i<strlen($str);$i++)
-	{
-		$e = ord($str{$i});
-		$str{$i}=$e&0xE0?chr($e^$k[$j]):chr($e);
-		$j++;
-		$j=$j==$kl?0:$j;
-	}
-	return $str;
-}
+add_filter('the_content', array('encryptblog', 'decrypt_content'), 1, 1);
+add_filter('content_edit_pre', array('encryptblog', 'decrypt_content'), 1, 1);
+add_filter('content_save_pre', array('encryptblog', 'encrypt_content'), 1, 1);
+add_filter('login_redirect', array('encryptblog', 'eb_redirect'), 10, 3);
+add_filter('init', array('encryptblog', 'eb_startsession'), 1);
+add_action('wp_logout', array('encryptblog', 'eb_endsession'));
+add_action('wp_login', array('encryptblog', 'eb_endsession'));
 ?>
---
+++ readme.txt
@@ -0,0 +1,6 @@
+TODO
+
+Require uses be logged in to view site.
+On login redirect to form that user enters encoding key.
+Create table that tracks if entries are encoded or not.
+Add option to bulk encode or permantly decode entries.
